c6e7f75bd63fc49abe05170269efcde264447a19
Lokesh Redekar <redekarlk123@gmail.com>
2025-09-08 16:08:11 +0530

    state changes

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -10,6 +10,13 @@ const AuthPage = () => {
   const router = useRouter();
   const { backendUrl, setIsLoggedIn, getUserData } = useContext(AppContent);
 
+  // const [state, setState] = useState("Login");
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [loading, setLoading] = useState(false);
+
+
   const [state, setState] = useState("Login");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
